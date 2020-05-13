155af4647a95f6a055f714cdc8374ff219c1c93a
Jérôme Clament-Sanz <j.clamentsanz@gmail.com>
2020-05-13 11:58:09 +0200

    squelette

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,12 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.myapplication.fragments.AboutFragment
+import com.example.myapplication.fragments.QuestionsFragment
+import com.example.myapplication.fragments.SettingsFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +15,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun menuClick(view: View) {
+        when(view.id){
+            R.id.btnQuestions -> QuestionsFragment().load()
+            R.id.btnSettings -> SettingsFragment().load()
+            R.id.btnAbout -> AboutFragment().load()
+        }
+    }
+
+    private fun Fragment.load() =
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout, this).commit()
+
 }
